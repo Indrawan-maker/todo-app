89bb1f8f9333596ab784d7977e0cd0ba6ef10070
Indrawan <indrawanismail0@gmail.com>
2025-05-31 22:22:17 +0800

    add libsweetalert

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const icon = document.getElementById("icon")
 // darkmode
 icon.addEventListener('click', () => {
     document.body.classList.toggle("dark-theme")
-    if(document.body.classList.contains("dark-theme")) {
+    if (document.body.classList.contains("dark-theme")) {
         icon.src = "/images/sun.png"
     } else {
         icon.src = "/images/moon.png"
@@ -53,16 +53,16 @@ let currentMinutes = currentDateTime.getMinutes()
 let currentDate = currentDateTime.toDateString()
 
 if (currentHours >= 4 && currentHours <= 9) {
-        good.textContent = `Good Morning, ${name}!`
+    good.textContent = `Good Morning, ${name}!`
 } else if (currentHours >= 10 && currentHours <= 14) {
-        good.textContent = `Good Afternoon, ${name}!`
+    good.textContent = `Good Afternoon, ${name}!`
 } else if (currentHours >= 15 && currentHours <= 18) {
-        good.textContent = `Good Evening, ${name}!`
+    good.textContent = `Good Evening, ${name}!`
 } else {
-        good.textContent = `Good Night, ${name}!`
+    good.textContent = `Good Night, ${name}!`
 }
 function Time(num) {
-        return ("0" + num).slice(-2)
+    return ("0" + num).slice(-2)
 }
 let currentTime = Time(currentHours) + ":" + Time(currentMinutes)
 
@@ -70,8 +70,8 @@ time.textContent = currentTime
 day.textContent = currentDate
 
 // pengaturan todolist baru
-function addTodo(){
-    if(boxInput.value !== "") {
+function addTodo() {
+    if (boxInput.value !== "") {
         let newLi = document.createElement("li")
         newLi.innerHTML = boxInput.value
         liContainer.appendChild(newLi)
@@ -79,44 +79,57 @@ function addTodo(){
         span.innerHTML = "❌"
         newLi.appendChild(span)
     } else {
-        alert("masukan input!")
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+
     }
     boxInput.value = ""
     saveData()
 }
 
 // pengaturan edit todo
-liContainer.addEventListener("click", function (e){
-    if(e.target.tagName === "LI") {
+liContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked")
         saveData()
-    } else if (e.target.tagName ===  "SPAN") {
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove()
         saveData()
     }
-},false
+}, false
 )
 
 // fitur tekan enter 
-boxInput.addEventListener('keypress', function(e) {
-    if(e.key === 'Enter') {
+boxInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
         addTodo()
     }
 })
 
 
 // local storage
-function saveData () {
+function saveData() {
     localStorage.setItem("data", liContainer.innerHTML)
 }
 
-function showData () {
+function showData() {
     liContainer.innerHTML = localStorage.getItem("data")
 }
 
 function clearAll() {
     localStorage.clear()
     liContainer.innerHTML = ""
+    Swal.fire({
+        position: "middle",
+        icon: "success",
+        title: "All clear",
+        showConfirmButton: false,
+        timer: 1500
+    });
 }
 
 showData()
